76a30ecbd74dc471bcd1a235487ed103c9db0e8b
DIFoundation <adeniranibrahim165@gmail.com>
2025-04-03 08:18:38 +0100

    optimizing

--- script.js
+++ script.js
@@ -96,3 +96,11 @@ function toggleText() {
     btnText.innerHTML = "Read more";
   }
 }
+
+function openPopup(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closePopup(id) {
+  document.getElementById(id).style.display = "none";
+}
